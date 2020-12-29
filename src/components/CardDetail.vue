e89c70d4e43ed1791b6fcb00676f5ab0091812a5
刘杨 <1060376291@qq.com>
2020-12-30 07:14:47 +0800

    '增加了分类'

---
+++ src/components/CardDetail.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "CardDetail"
+}
+</script>
+
+<style lang="less">
+
+</style>
---
+++ src/components/Report/ReportView.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "ReportView"
+}
+</script>
+
+<style lang="less">
+
+</style>
